bd0d6b823656267bb9d22075f3858046d286635d
Borislove <abirme@yandex.ru>
2021-02-03 19:07:48 +0300

    first program)

---
+++ src/MyFirstProgram.java
@@ -0,0 +1,22 @@
+//pizza mmmMMmmm
+public class MyFirstProgram {
+
+    public static void main(String[] args) {
+        System.out.print("I love pizza");
+        System.out.print("It's really good!");
+
+        System.out.print("I love pizza\n");  //с новой строки
+        System.out.print("It's really good!");
+
+        System.out.println("I love pizza\n");
+        System.out.println("It's really good!");
+
+        System.out.println("\tI love pizza");  //табуляция
+
+        System.out.println("\"I love pizza\"");  //кавычки
+
+        System.out.println("\\I love pizza");
+
+        //This is a comment
+    }
+}
