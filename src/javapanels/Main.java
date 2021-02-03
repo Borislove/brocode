72c9fb580640a341cdd8fa8478189c5a2a645270
Borislove <abirme@yandex.ru>
2021-02-03 04:02:27 +0300

    Java panels

---
+++ src/javapanels/Main.java
@@ -0,0 +1,43 @@
+package javapanels;
+
+import javax.swing.*;
+import java.awt.*;
+
+//Java panels
+public class Main {
+    public static void main(String[] args) {
+
+        ImageIcon icon = new ImageIcon("e:\\javadoc\\test.jpg");
+
+        JLabel label = new JLabel();
+        label.setText("Hi");
+        label.setIcon(icon);
+        label.setVerticalTextPosition(JLabel.BOTTOM);
+        label.setHorizontalTextPosition(JLabel.RIGHT);
+
+
+        JPanel redPanel = new JPanel();
+        redPanel.setBackground(Color.red);
+        redPanel.setBounds(0,0,250,250);
+
+        JPanel bluePanel = new JPanel();
+        bluePanel.setBackground(Color.blue);
+        bluePanel.setBounds(250,0,250,250);
+
+        JPanel greenPanel = new JPanel();
+        greenPanel.setBackground(Color.green);
+        greenPanel.setBounds(0,250,500,250);
+        greenPanel.setLayout(new BorderLayout());
+
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setLayout(null);
+        frame.setSize(750,750);
+        frame.setVisible(true);
+
+        greenPanel.add(label);
+        frame.add(redPanel);
+        frame.add(bluePanel);
+        frame.add(greenPanel);
+    }
+}
