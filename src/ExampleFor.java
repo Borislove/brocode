035d1d0a5c30d745a8bc6ffaf371ba1439f9491a
Borislove <abirme@yandex.ru>
2021-02-04 06:16:29 +0300

    Java for loop

---
+++ src/ExampleFor.java
@@ -0,0 +1,15 @@
+public class ExampleFor {
+    public static void main(String[] args) {
+
+        // for loop = executes a block of code a limited amount of times
+
+        /*for(int i = 0; i<=10; i++){
+            System.out.println(i);
+        }*/
+
+        for(int i = 10; i>=0; i--){
+            System.out.println(i);
+        }
+        System.out.println("Happy new Year!");
+    }
+}
