741746e4c602cbfefff3391a92bb069ae48a9885
Borislove <abirme@yandex.ru>
2021-02-05 16:16:40 +0300

    Java String methods

---
+++ src/ExampleStringMethods.java
@@ -0,0 +1,22 @@
+public class ExampleStringMethods {
+    public static void main(String[] args) {
+        // String = a reference data type that can store one or more characters
+        //   reference data types have access to useful methods
+        String name = "name    ";
+        /*boolean result = name.equals("name");
+        System.out.println(result);
+
+        boolean resultIgnored = name.equalsIgnoreCase("Name");
+        System.out.println("resultIgnored = " + resultIgnored);*/
+
+        //int result = name.length();
+        //char result = name.charAt(0);
+        //int result = name.indexOf("m");  //узнать индекс где находится символ
+        //boolean result = name.isEmpty(); //присвоить пустоту?
+        //  String result = name.toUpperCase();
+        //String result = name.toLowerCase();
+        //String result= name.trim(); //удалить пробелы
+        //String result = name.replace('m', 'a'); //заменить один символ на другой
+//        System.out.println(result);
+    }
+}
