37e09fd570fd918adf946bf5306d829e3cbd1fac
Borislove <abirme@yandex.ru>
2021-02-06 11:22:35 +0300

    Java final keyword

---
+++ src/ExampleFinal.java
@@ -0,0 +1,8 @@
+public class ExampleFinal {
+    public static void main(String[] args) {
+
+       final double PI = 3.14159;
+      //  pi = 4; //You can't change a final variable
+        System.out.println(PI);
+    }
+}
