080892f9053fbc684ef90278e0b2bb1654c8820f
Borislove <abirme@yandex.ru>
2021-02-08 05:01:45 +0300

    Java polymorphism

---
+++ src/OOP/polymorphism/Main.java
@@ -0,0 +1,23 @@
+package OOP.polymorphism;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // polymorphism = 	greek word for poly-"many", morph-"form"
+        //					The ability of an object to identify as more than one type
+
+        Car car = new Car();
+        Bicycle bicycle = new Bicycle();
+        Boat boat = new Boat();
+
+        Vehicle[] racers = {car,bicycle,boat};
+
+//        car.go();
+//        bicycle.go();
+//        boat.go();
+
+        for(Vehicle x: racers){
+            x.go();
+        }
+    }
+}
---
+++ src/OOP/polymorphism/Vehicle.java
@@ -0,0 +1,7 @@
+package OOP.polymorphism;
+
+public class Vehicle {
+public void go(){
+
+}
+}
