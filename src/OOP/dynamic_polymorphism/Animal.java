500835c40e058cdf0eb760216280baa48ede1e84
Borislove <abirme@yandex.ru>
2021-02-08 05:48:58 +0300

    Java dynamic polymorphism

---
+++ src/OOP/dynamic_polymorphism/Animal.java
@@ -0,0 +1,7 @@
+package OOP.dynamic_polymorphism;
+
+public class Animal {
+    public void speak(){
+        System.out.println("animal goes *brrrr*");
+    }
+}
---
+++ src/OOP/dynamic_polymorphism/Dog.java
@@ -0,0 +1,8 @@
+package OOP.dynamic_polymorphism;
+
+public class Dog extends Animal {
+    @Override
+    public void speak(){
+        System.out.println("dog goes *bark*");
+    }
+}
