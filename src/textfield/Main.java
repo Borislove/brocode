2961ed79511fcf7227d49dc88e1d6f4b11f1431c
Borislove <abirme@yandex.ru>
2021-02-10 14:44:21 +0300

    Java textfield

---
+++ src/textfield/Main.java
@@ -0,0 +1,8 @@
+package textfield;
+
+public class Main {
+    public static void main(String[] args) {
+        // JTextField = A GUI textbox component that can be used to add, set, or get text
+        MyFrame frame = new MyFrame();
+    }
+}
---
+++ src/textfield/MyFrame.java
@@ -0,0 +1,46 @@
+package textfield;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MyFrame extends JFrame implements ActionListener {
+
+    JButton button;
+    JTextField textField;
+
+    MyFrame() {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(new FlowLayout());
+
+        // JButton button = new JButton("Submit");
+        button = new JButton("Submit");
+        button.addActionListener(this);
+
+        //JTextField textField = new JTextField();
+        textField = new JTextField();
+        textField.setPreferredSize(new Dimension(250, 40));
+        textField.setFont(new Font("Consolas", Font.PLAIN, 35));
+        textField.setForeground(new Color(0x00FF00));
+        textField.setBackground(Color.black);
+        textField.setCaretColor(Color.orange); //каретка
+        textField.setText("username: ");
+     //   textField.setEditable(false); //редактирование
+
+        this.add(button);
+        this.add(textField);
+        this.pack();
+        this.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == button) {
+            System.out.println("Welcome " + textField.getText());
+            button.setEnabled(false);
+            textField.setEditable(false);
+            //textField.getText();
+        }
+    }
+}
