857126eb235fc811a57a9d51a4c2fd17ed39e56d
Borislove <abirme@yandex.ru>
2021-02-13 12:40:40 +0300

    Java drag and drop

---
+++ src/DragAndDrop/DragPanel.java
@@ -0,0 +1,48 @@
+package DragAndDrop;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
+
+public class DragPanel extends JPanel {
+
+    ImageIcon image = new ImageIcon("E:\\javadoc\\frolic.png");
+    final int WIDTH = image.getIconWidth();
+    final int HEIGHT = image.getIconHeight();
+    Point imageCorner;
+    Point prevPt;
+
+    DragPanel() {
+        imageCorner = new Point(0, 0);
+        ClickListener clickListener = new ClickListener();
+        DragListener dragListener = new DragListener();
+
+        this.addMouseListener(clickListener);
+        this.addMouseMotionListener(dragListener);
+    }
+
+    public void painComponent(Graphics g) {
+        super.paintComponent(g);
+        image.paintIcon(this, g, (int) imageCorner.getX(), (int) imageCorner.getY());
+    }
+
+    private class ClickListener extends MouseAdapter {
+        public void mousePressed(MouseEvent e) {
+            prevPt = e.getPoint();
+        }
+    }
+
+    private class DragListener extends MouseMotionAdapter {
+        public void mouseDragged(MouseEvent e) {
+            Point currentPt = e.getPoint();
+            imageCorner.translate(
+                    (int) (currentPt.getX() - prevPt.getX()),
+                    (int) (currentPt.getY() - prevPt.getY())
+            );
+            prevPt = currentPt;
+            repaint();
+        }
+    }
+}
---
+++ src/DragAndDrop/Main.java
@@ -0,0 +1,8 @@
+package DragAndDrop;
+
+public class Main {
+    public static void main(String[] args) {
+     new MyFrame();
+
+    }
+}
