42cd2a808fec72158cea862095e7101e786b7809
Borislove <abirme@yandex.ru>
2021-02-14 13:10:35 +0300

    Java inner classes

---
+++ src/innerclass/Outside.java
@@ -0,0 +1,14 @@
+package innerclass;
+
+public class Outside {
+
+    String x = "Hello ";
+
+    public class Inside {
+        String y = "World!";
+
+        public void Greeting(){
+            System.out.println(x+y);
+        }
+    }
+}
