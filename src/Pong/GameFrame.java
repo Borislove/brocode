4eacbf8c25e15091ae9525ed102c53a69e463541
Borislove <abirme@yandex.ru>
2021-02-20 21:19:37 +0300

    Java pong game

---
+++ src/Pong/GameFrame.java
@@ -0,0 +1,21 @@
+package Pong;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GameFrame extends JFrame {
+
+    GamePanel panel;
+
+    GameFrame() {
+        panel = new GamePanel();
+        this.add(panel);
+        this.setTitle("Pong Game");
+        this.setResizable(false);
+        this.setBackground(Color.black);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.pack();
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+    }
+}
---
+++ src/Pong/PongGame.java
@@ -0,0 +1,8 @@
+package Pong;
+
+public class PongGame {
+    public static void main(String[] args) {
+        //GameFrame frame = new GameFrame();
+        new GameFrame();
+    }
+}
